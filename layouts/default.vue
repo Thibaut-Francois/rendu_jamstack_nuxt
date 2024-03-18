351fc53f14e0bffa90d1af554dfb283dd4461262
Vorn-Hagen <thibaut.francois@edu.devinci.fr>
2024-03-18 15:32:59 +0100

    exo 2

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <main role="main">
+        <header>
+            <nav>The nav</nav>
+        </header>
+        <slot />
+        <footer>
+            <p>© 2021</p>
+        </footer>
+    </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+    const { find } = useStrapi()
+
+    const { data, pending, error } = await useAsyncData('players', async () => {
+        return await find('players', {
+            populate: '*',
+        })
+    })
+</script>
+
+<template>
+    <TheHeader />
+    <h1>Hello world</h1>
+    <About />
+</template>
+
+<style scoped></style>
