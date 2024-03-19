bcf397ce5a2f5aab13f03acdb9720fc93558f9a8
Vorn-Hagen <thibaut.francois@edu.devinci.fr>
2024-03-19 09:38:40 +0100

    exo 3

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
 
-    const { find } = useStrapi()
+   const { find } = useStrapi()
 
-    const { data, pending, error } = await useAsyncData('players', async () => {
-        return await find('players', {
-            populate: '*',
-        })
-    })
+   const { data, pending, error } = await useAsyncData('players', async () => {
+       return await find('players', {
+            populate: '*'
+       }).then(res => res.data)
+   })
+
 </script>
 
 <template>
-    <TheHeader />
-    <h1>Hello world</h1>
-    <About />
+    <h1>Hello World</h1>
+    <section>
+        <a :href="`/players/${player.slug}`" v-for="player in data" :key="player.slug">{{ player.name }}</a>
+    </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+    h1 {
+        color: red;
+    }
+    section {
+        display: flex;
+        flex-direction: column;
+    }
+    a {
+        margin: 10px 0;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+</style>
---
+++ pages/players/[...slug].vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const { findOne } = useStrapi4()
+const route = useRoute()
+
+const {data, pending} = useAsyncData('player', () => findOne(`players/${route.params.slug}`))
+
+
+</script>
+
+<template>
+    <h1>OUI</h1>
+    <pre>{{ data }}</pre>
+</template>
